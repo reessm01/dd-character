de0fbaf07d8be1325b9cb34f912fb1e0d293c15f
Scott Reese <scott_reese@pm.me>
2021-01-01 19:33:44 -0500

    deploy ready

--- src/helpers/getEnvironmentDetails.ts
+++ src/helpers/getEnvironmentDetails.ts
@@ -1,7 +1,7 @@
 import { Config, Environments, environments } from '../environments/environments';
 
 export const getEnvironmentDetails = (): Config => {
-    const env = process.env.REACT_APP_ENV as keyof Environments;
+    const env = ((process.env.REACT_APP_ENV as string) as keyof Environments) || ('local' as keyof Environments);
 
     return environments[env];
 };
