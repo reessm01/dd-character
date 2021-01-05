2553c7fbd3450df4ebf2e7e1d792d089dbd827f7
Scott Reese <scott_reese@pm.me>
2021-01-05 09:55:07 -0500

    added spinner & passed spellSelected hook to child

--- src/pages/Search/GenericSearchPage.tsx
+++ src/pages/Search/GenericSearchPage.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import './GenericSearchPage.scss';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Spinner } from 'react-bootstrap';
 import { SearchableInput, SearchableToggleProps } from '../../components';
 import { useSpellList } from '../../hooks';
 import { Dnd5eApiPreview, Spell } from '../../interfaces';
 import { useSpell } from '../../hooks';
-import { SpellCard } from '../../components/SpellCard/SpellCard';
+import { SpellCard } from '../../components';
 
 export function GenericSearchPage() {
     const initSpellSelected: Dnd5eApiPreview = {
@@ -13,8 +13,8 @@ export function GenericSearchPage() {
         name: 'Acid Arrow',
         url: '/api/spells/acid-arrow',
     };
-    const { spellList, isSpellListLoading, spellListError } = useSpellList();
     const [spellSelected, setSpellSelected] = useState<Dnd5eApiPreview>(initSpellSelected);
+    const { spellList, isSpellListLoading, spellListError } = useSpellList();
     const { spell, isSpellLoading, spellError } = useSpell(spellSelected);
 
     const inputOptions: SearchableToggleProps<Dnd5eApiPreview> = {
@@ -40,11 +40,20 @@ export function GenericSearchPage() {
                         suggestionKey={inputOptions.suggestionKey}
                         handleSelect={inputOptions.handleSelect}
                     />
+                    {isSpellListLoading ||
+                        (isSpellLoading && (
+                            <>
+                                <Spinner animation="border" role="status" className="mt-3 mr-3">
+                                    <span className="sr-only">Loading...</span>
+                                </Spinner>
+                                <span>Loading details...</span>
+                            </>
+                        ))}
                 </Col>
             </Row>
             <Row>
                 <Col className="mt-3 d-flex justify-content-center">
-                    <SpellCard spell={spell as Spell} />
+                    <SpellCard spell={spell as Spell} spellSelected={spellSelected} />
                 </Col>
             </Row>
         </Container>
