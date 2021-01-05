e6b17bc76a262a6e70e6770f5f7aafdea170ebcb
Scott Reese <scott_reese@pm.me>
2021-01-05 09:56:20 -0500

    removed unused variables

--- src/helpers/getIcons.ts
+++ src/helpers/getIcons.ts
@@ -1,14 +1,10 @@
-import { getEnvironmentDetails } from './index';
 import { Icon } from '../interfaces';
 
 export const getIcon = (school: string) => {
-    const environment = getEnvironmentDetails();
-    const token = environment.iconFinderApiKey;
-
     const initIconList: Icon[] = [
         { school: 'evocation', url: '' },
         { school: 'illusion', url: '' },
-        { school: 'necromancy', url: 'https://api.iconfinder.com/v4/icons/7112664/formats/png/32/download?token=' },
+        { school: 'necromancy', url: 'https://api.iconfinder.com/v4/icons/7112664/formats/png/32/download' },
         { school: 'enchantment', url: '' },
         { school: 'divination', url: '' },
         { school: 'transmutation', url: '' },
@@ -16,7 +12,7 @@ export const getIcon = (school: string) => {
         { school: 'abjuration', url: '' },
     ];
 
-    const masterIconList = initIconList.map((icon) => ({ ...icon, url: `${icon.url}${token}` }));
+    const masterIconList = initIconList.map((icon) => ({ ...icon, url: `${icon.url}` }));
 
     return masterIconList.find((icon) => icon.school === school)?.url || '';
 };
